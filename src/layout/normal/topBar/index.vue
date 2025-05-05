ae3e7ab2c945f4f885c491eca68fc4224b35a20c
Happier <zhf5256@qq.com>
2025-05-05 10:24:01 +0800

    fix: 移除无用代码

--- src/layout/normal/topBar/index.vue
+++ src/layout/normal/topBar/index.vue
@@ -25,25 +25,6 @@
           <GoForwardIcon size="18px"></GoForwardIcon>
         </button>
       </div>
-      <!-- <label class="input">
-        <svg
-          class="h-[1em] opacity-50"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-        >
-          <g
-            stroke-linejoin="round"
-            stroke-linecap="round"
-            stroke-width="2.5"
-            fill="none"
-            stroke="currentColor"
-          >
-            <circle cx="11" cy="11" r="8"></circle>
-            <path d="m21 21-4.3-4.3"></path>
-          </g>
-        </svg>
-        <input type="search" required placeholder="搜索" />
-      </label> -->
     </div>
     <div
       class="w-full h-full flex items-center justify-end"
