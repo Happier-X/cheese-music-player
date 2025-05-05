bbed348bdaac9f5b7048afa4f46b525f2f29582b
Happier <zhf5256@qq.com>
2025-05-05 17:10:49 +0800

    feat: 调整布局

--- src/layout/normal/index.vue
+++ src/layout/normal/index.vue
@@ -1,29 +1,27 @@
 <template>
-  <div class="w-full h-full">
-    <div class="w-full h-[calc(100%-75px)] flex">
-      <div class="w-[200px] h-full">
-        <SideBar></SideBar>
-      </div>
-      <div class="w-[calc(100%-200px)] h-full">
-        <div class="w-full h-[60px]">
-          <TopBar></TopBar>
-        </div>
-        <div class="w-full h-[calc(100%-60px)] p-[10px]">
-          <RouterView v-slot="{ Component }">
-            <KeepAlive>
-              <component :is="Component" />
-            </KeepAlive>
-          </RouterView>
-        </div>
-      </div>
+  <div class="w-full h-full flex bg-base-100">
+    <div class="w-[65px] h-full bg-base-200">
+      <SideBar></SideBar>
     </div>
-    <div class="w-full h-[75px]">
-      <!-- <PlayBar></PlayBar> -->
+    <div class="w-[calc(100%-65px)] h-full">
+      <div class="w-full h-[60px]">
+        <TopBar></TopBar>
+      </div>
+      <div class="w-full h-[calc(100%-135px)]">
+        <RouterView v-slot="{ Component }">
+          <KeepAlive>
+            <component :is="Component"></component>
+          </KeepAlive>
+        </RouterView>
+      </div>
+      <div class="w-full h-[75px]">
+        <PlayBar></PlayBar>
+      </div>
     </div>
   </div>
 </template>
 <script setup lang="ts">
 import SideBar from "./sideBar/index.vue";
 import TopBar from "./topBar/index.vue";
-// import PlayBar from "./playBar/index.vue";
+import PlayBar from "./playBar/index.vue";
 </script>
---
+++ src/layout/normal/playBar/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="w-full h-full p-2 flex items-center justify-between gap-3">
+    <div class="w-full h-full">
+      <!-- <MusicInfo
+                :cover-img="playerStore.currentSongInfo?.coverImg"
+                :title="playerStore.currentSongInfo?.title"
+                :artist="playerStore.currentSongInfo?.artist"
+                :album="playerStore.currentSongInfo?.album"></MusicInfo> -->
+      <MusicInfo></MusicInfo>
+    </div>
+    <div class="w-full h-full">
+      <PlayControl></PlayControl>
+    </div>
+    <div class="w-full h-full">
+      <PlayTool></PlayTool>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+import MusicInfo from "./components/MusicInfo.vue";
+import PlayControl from "./components/PlayControl.vue";
+import PlayTool from "./components/PlayTool.vue";
+// import { usePlayerStore } from '@renderer/stores/player'
+// // 播放器状态管理
+// const playerStore = usePlayerStore()
+</script>
