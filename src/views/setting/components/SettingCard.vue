d397c69e85176528b5a2b2044916ccf83cca9ca5
Happier <zhf5256@qq.com>
2025-05-07 22:37:26 +0800

    feat: 增加设置页面

---
+++ src/views/setting/components/SettingCard.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="card shadow">
+    <div class="card-body">
+      <div class="card-title">媒体库</div>
+      <div class="flex flex-col gap-3">
+        <div class="flex items-center justify-between">
+            <div>未设置</div>
+            <div><button class="btn">设置</button></div>
+        </div>
+        <div>连接模式</div>
+      </div>
+    </div>
+  </div>
+</template>
+<script setup lang="ts"></script>
