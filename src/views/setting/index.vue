c2ed689b8ebe55849183a3df666ab0378d453c85
赵鸿飞 <zhaohongfei@vasoyn.com>
2025-05-08 14:57:57 +0800

    feat: 完成媒体库部分页面

--- src/views/setting/index.vue
+++ src/views/setting/index.vue
@@ -1,9 +1,16 @@
 <template>
   <div class="w-full h-full">
-    <SettingCard></SettingCard>
-    <SettingCard></SettingCard>
+    <SettingCard title="媒体库">
+      <div class="flex items-center justify-between">
+        <div>未设置</div>
+        <button class="btn btn-sm btn-circle btn-ghost">
+          <EditIcon size="20px"></EditIcon>
+        </button>
+      </div>
+    </SettingCard>
   </div>
 </template>
 <script setup lang="ts">
 import SettingCard from "./components/SettingCard.vue";
+import { RiEditLine as EditIcon } from "@remixicon/vue";
 </script>
