c0d6611e8f523594f92963f0441a3f7e607d33a1
赵鸿飞 <zhaohongfei@vasoyn.com>
2025-05-08 17:28:43 +0800

    feat: 完成服务配置表单

---
+++ src/views/setting/components/MediaLibrarySetting.vue
@@ -0,0 +1,46 @@
+<template>
+  <CheeseCard title="媒体库">
+    <div class="flex items-center justify-between">
+      <div>未设置</div>
+      <button
+        class="btn btn-sm btn-circle btn-ghost"
+        @click="visible = !visible"
+      >
+        <EditIcon size="20px"></EditIcon>
+      </button>
+    </div>
+  </CheeseCard>
+  <CheeseDialog v-model="visible" title="媒体库配置">
+    <fieldset class="fieldset">
+      <label class="floating-label mt-5">
+        <span>服务地址</span>
+        <input
+          type="text"
+          class="input input-md w-full"
+          placeholder="服务地址"
+        />
+      </label>
+      <label class="floating-label mt-5">
+        <span>用户名</span>
+        <input type="text" class="input input-md w-full" placeholder="用户名" />
+      </label>
+      <label class="floating-label mt-5">
+        <span>密码</span>
+        <input
+          type="password"
+          class="input input-md w-full"
+          placeholder="密码"
+        />
+      </label>
+      <button class="btn btn-neutral mt-5">保存</button>
+    </fieldset>
+  </CheeseDialog>
+</template>
+<script setup lang="ts">
+import CheeseCard from "../../../components/CheeseCard.vue";
+import CheeseDialog from "../../../components/CheeseDialog.vue";
+import { RiEditLine as EditIcon } from "@remixicon/vue";
+import { ref } from "vue";
+
+const visible = ref(false);
+</script>
--- src/views/setting/index.vue
+++ src/views/setting/index.vue
@@ -1,21 +1,8 @@
 <template>
   <div class="w-full h-full">
-    <CheeseCard title="媒体库">
-      <div class="flex items-center justify-between">
-        <div>未设置</div>
-        <button class="btn btn-sm btn-circle btn-ghost" @click="test = !test">
-          <EditIcon size="20px"></EditIcon>
-        </button>
-      </div>
-    </CheeseCard>
+    <MediaLibrarySetting></MediaLibrarySetting>
   </div>
-  <CheeseDialog v-model="test"></CheeseDialog>
 </template>
 <script setup lang="ts">
-import CheeseCard from "../../components/CheeseCard.vue";
-import CheeseDialog from "../../components/CheeseDialog.vue";
-import { RiEditLine as EditIcon } from "@remixicon/vue";
-import { ref } from "vue";
-
-const test = ref(false);
+import MediaLibrarySetting from "./components/MediaLibrarySetting.vue";
 </script>
